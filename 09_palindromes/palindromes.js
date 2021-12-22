c6ec1c158cd03b462bc7a7c5b1226e73143f9fdb
Budescu Eftimie <budescueftimie@gmail.com>
2021-12-22 05:31:28 +0100

    done palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (someString) {
+  //remove  punctuation, spaces and convert to lowerCase
+  someString = someString
+    .replace(/[^\w\s]|_/g, "")
+    .replace(/\s+/g, "")
+    .toLowerCase();
+  // transform string to array and reverse array then join back into a string
+  let reverseString = someString.split("").reverse().join("");
+  // Check if strings are palindroms ? true:false
+  let palindromes = reverseString === someString ? true : false;
+  return palindromes;
 };
 
 // Do not edit below this line
