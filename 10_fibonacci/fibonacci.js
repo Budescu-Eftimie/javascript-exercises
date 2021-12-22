7cf7bb1820d71164d7f2fee714a6df1ccbfb9fd8
Budescu Eftimie <budescueftimie@gmail.com>
2021-12-22 06:12:01 +0100

    done fibonnaci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  let array = [1, 1];
+  for (i = 2; i < n; i++) {
+    let element = array.slice(array.length - 2).reduce((a, b) => a + b, 0);
+    array.push(element);
+  }
+  let number = array.slice(-1)[0];
+  if (n > 0) {
+    return number;
+  } else return "OOPS";
 };
 
 // Do not edit below this line
